2cb91d7c443f0e2853a2800ea4e78e9c41f514da
roman <root@localhost>
2017-04-10 13:07:29 +0400

    replaced chrome by nightmare

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -95,7 +95,7 @@ module.exports = function(config) {
      * available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
      */
     browsers: [
-      'Chrome'
+      'Nightmare'
     ],
 
     customLaunchers: {
